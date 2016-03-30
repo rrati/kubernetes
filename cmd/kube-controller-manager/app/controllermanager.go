0315230151ce66df47067740db5d52f1c0629bec
Robert Rati <rrati@redhat.com>
2016-03-30 10:07:30 -0400

    Added controller startup delay

--- cmd/kube-controller-manager/app/controllermanager.go
+++ cmd/kube-controller-manager/app/controllermanager.go
@@ -74,6 +74,17 @@ import (
 	"github.com/spf13/pflag"
 )
 
+const (
+	// How long to wait before starting the next controller
+	controllerStartInterval = time.Duration(1 * time.Second)
+
+	// jitter factor for controllerStartInterval
+	controllerStartJitterFactor = 0.5
+
+	// jitter factor for controller resyncIntervals
+//	controllerResyncJitterFactor = 0.5
+)
+
 // NewControllerManagerCommand creates a *cobra.Command object with default parameters
 func NewControllerManagerCommand() *cobra.Command {
 	s := options.NewCMServer()
@@ -185,6 +196,7 @@ func Run(s *options.CMServer) error {
 func StartControllers(s *options.CMServer, kubeClient *client.Client, kubeconfig *restclient.Config, stop <-chan struct{}) error {
 	go endpointcontroller.NewEndpointController(clientset.NewForConfigOrDie(restclient.AddUserAgent(kubeconfig, "endpoint-controller")), ResyncPeriod(s)).
 		Run(s.ConcurrentEndpointSyncs, wait.NeverStop)
+	time.Sleep(wait.Jitter(controllerStartInterval, controllerStartJitterFactor))
 
 	go replicationcontroller.NewReplicationManager(
 		clientset.NewForConfigOrDie(restclient.AddUserAgent(kubeconfig, "replication-controller")),
@@ -192,11 +204,13 @@ func StartControllers(s *options.CMServer, kubeClient *client.Client, kubeconfig
 		replicationcontroller.BurstReplicas,
 		s.LookupCacheSizeForRC,
 	).Run(s.ConcurrentRCSyncs, wait.NeverStop)
+	time.Sleep(wait.Jitter(controllerStartInterval, controllerStartJitterFactor))
 
 	if s.TerminatedPodGCThreshold > 0 {
 		go gc.New(clientset.NewForConfigOrDie(restclient.AddUserAgent(kubeconfig, "garbage-collector")), ResyncPeriod(s), s.TerminatedPodGCThreshold).
 			Run(wait.NeverStop)
 	}
+	time.Sleep(wait.Jitter(controllerStartInterval, controllerStartJitterFactor))
 
 	cloud, err := cloudprovider.InitCloudProvider(s.CloudProvider, s.CloudConfigFile)
 	if err != nil {
@@ -210,11 +224,13 @@ func StartControllers(s *options.CMServer, kubeClient *client.Client, kubeconfig
 		util.NewTokenBucketRateLimiter(s.DeletingPodsQps, s.DeletingPodsBurst),
 		s.NodeMonitorGracePeriod.Duration, s.NodeStartupGracePeriod.Duration, s.NodeMonitorPeriod.Duration, clusterCIDR, s.AllocateNodeCIDRs)
 	nodeController.Run(s.NodeSyncPeriod.Duration)
+	time.Sleep(wait.Jitter(controllerStartInterval, controllerStartJitterFactor))
 
 	serviceController := servicecontroller.New(cloud, clientset.NewForConfigOrDie(restclient.AddUserAgent(kubeconfig, "service-controller")), s.ClusterName)
 	if err := serviceController.Run(s.ServiceSyncPeriod.Duration, s.NodeSyncPeriod.Duration); err != nil {
 		glog.Errorf("Failed to start service controller: %v", err)
 	}
+	time.Sleep(wait.Jitter(controllerStartInterval, controllerStartJitterFactor))
 
 	if s.AllocateNodeCIDRs {
 		if cloud == nil {
@@ -224,6 +240,7 @@ func StartControllers(s *options.CMServer, kubeClient *client.Client, kubeconfig
 		} else {
 			routeController := routecontroller.New(routes, clientset.NewForConfigOrDie(restclient.AddUserAgent(kubeconfig, "route-controller")), s.ClusterName, clusterCIDR)
 			routeController.Run(s.NodeSyncPeriod.Duration)
+			time.Sleep(wait.Jitter(controllerStartInterval, controllerStartJitterFactor))
 		}
 	} else {
 		glog.Infof("allocate-node-cidrs set to %v, node controller not creating routes", s.AllocateNodeCIDRs)
@@ -248,6 +265,7 @@ func StartControllers(s *options.CMServer, kubeClient *client.Client, kubeconfig
 		GroupKindsToReplenish:     groupKindsToReplenish,
 	}
 	go resourcequotacontroller.NewResourceQuotaController(resourceQuotaControllerOptions).Run(s.ConcurrentResourceQuotaSyncs, wait.NeverStop)
+	time.Sleep(wait.Jitter(controllerStartInterval, controllerStartJitterFactor))
 
 	// If apiserver is not running we should wait for some time and fail only then. This is particularly
 	// important when we start apiserver and controller manager at the same time.
@@ -278,6 +296,7 @@ func StartControllers(s *options.CMServer, kubeClient *client.Client, kubeconfig
 	}
 	namespaceController := namespacecontroller.NewNamespaceController(namespaceKubeClient, namespaceClientPool, groupVersionResources, s.NamespaceSyncPeriod.Duration, api.FinalizerKubernetes)
 	go namespaceController.Run(s.ConcurrentNamespaceSyncs, wait.NeverStop)
+	time.Sleep(wait.Jitter(controllerStartInterval, controllerStartJitterFactor))
 
 	groupVersion := "extensions/v1beta1"
 	resources, found := resourceMap[groupVersion]
@@ -296,30 +315,35 @@ func StartControllers(s *options.CMServer, kubeClient *client.Client, kubeconfig
 			)
 			go podautoscaler.NewHorizontalController(hpaClient.Core(), hpaClient.Extensions(), hpaClient, metricsClient, s.HorizontalPodAutoscalerSyncPeriod.Duration).
 				Run(wait.NeverStop)
+			time.Sleep(wait.Jitter(controllerStartInterval, controllerStartJitterFactor))
 		}
 
 		if containsResource(resources, "daemonsets") {
 			glog.Infof("Starting daemon set controller")
 			go daemon.NewDaemonSetsController(clientset.NewForConfigOrDie(restclient.AddUserAgent(kubeconfig, "daemon-set-controller")), ResyncPeriod(s), s.LookupCacheSizeForDaemonSet).
 				Run(s.ConcurrentDaemonSetSyncs, wait.NeverStop)
+			time.Sleep(wait.Jitter(controllerStartInterval, controllerStartJitterFactor))
 		}
 
 		if containsResource(resources, "jobs") {
 			glog.Infof("Starting job controller")
 			go job.NewJobController(clientset.NewForConfigOrDie(restclient.AddUserAgent(kubeconfig, "job-controller")), ResyncPeriod(s)).
 				Run(s.ConcurrentJobSyncs, wait.NeverStop)
+			time.Sleep(wait.Jitter(controllerStartInterval, controllerStartJitterFactor))
 		}
 
 		if containsResource(resources, "deployments") {
 			glog.Infof("Starting deployment controller")
 			go deployment.NewDeploymentController(clientset.NewForConfigOrDie(restclient.AddUserAgent(kubeconfig, "deployment-controller")), ResyncPeriod(s)).
 				Run(s.ConcurrentDeploymentSyncs, wait.NeverStop)
+			time.Sleep(wait.Jitter(controllerStartInterval, controllerStartJitterFactor))
 		}
 
 		if containsResource(resources, "replicasets") {
 			glog.Infof("Starting ReplicaSet controller")
 			go replicaset.NewReplicaSetController(clientset.NewForConfigOrDie(restclient.AddUserAgent(kubeconfig, "replicaset-controller")), ResyncPeriod(s), replicaset.BurstReplicas, s.LookupCacheSizeForRS).
 				Run(s.ConcurrentRSSyncs, wait.NeverStop)
+			time.Sleep(wait.Jitter(controllerStartInterval, controllerStartJitterFactor))
 		}
 	}
 
@@ -331,6 +355,7 @@ func StartControllers(s *options.CMServer, kubeClient *client.Client, kubeconfig
 
 	pvclaimBinder := persistentvolumecontroller.NewPersistentVolumeClaimBinder(clientset.NewForConfigOrDie(restclient.AddUserAgent(kubeconfig, "persistent-volume-binder")), s.PVClaimBinderSyncPeriod.Duration)
 	pvclaimBinder.Run()
+	time.Sleep(wait.Jitter(controllerStartInterval, controllerStartJitterFactor))
 
 	pvRecycler, err := persistentvolumecontroller.NewPersistentVolumeRecycler(
 		clientset.NewForConfigOrDie(restclient.AddUserAgent(kubeconfig, "persistent-volume-recycler")),
@@ -343,6 +368,7 @@ func StartControllers(s *options.CMServer, kubeClient *client.Client, kubeconfig
 		glog.Fatalf("Failed to start persistent volume recycler: %+v", err)
 	}
 	pvRecycler.Run()
+	time.Sleep(wait.Jitter(controllerStartInterval, controllerStartJitterFactor))
 
 	if provisioner != nil {
 		pvController, err := persistentvolumecontroller.NewPersistentVolumeProvisionerController(persistentvolumecontroller.NewControllerClient(clientset.NewForConfigOrDie(restclient.AddUserAgent(kubeconfig, "persistent-volume-provisioner"))), s.PVClaimBinderSyncPeriod.Duration, s.ClusterName, volumePlugins, provisioner, cloud)
@@ -350,6 +376,7 @@ func StartControllers(s *options.CMServer, kubeClient *client.Client, kubeconfig
 			glog.Fatalf("Failed to start persistent volume provisioner controller: %+v", err)
 		}
 		pvController.Run()
+		time.Sleep(wait.Jitter(controllerStartInterval, controllerStartJitterFactor))
 	}
 
 	var rootCA []byte
@@ -378,6 +405,7 @@ func StartControllers(s *options.CMServer, kubeClient *client.Client, kubeconfig
 					RootCA:         rootCA,
 				},
 			).Run()
+			time.Sleep(wait.Jitter(controllerStartInterval, controllerStartJitterFactor))
 		}
 	}
 
@@ -385,6 +413,7 @@ func StartControllers(s *options.CMServer, kubeClient *client.Client, kubeconfig
 		clientset.NewForConfigOrDie(restclient.AddUserAgent(kubeconfig, "service-account-controller")),
 		serviceaccountcontroller.DefaultServiceAccountsControllerOptions(),
 	).Run()
+	time.Sleep(wait.Jitter(controllerStartInterval, controllerStartJitterFactor))
 
 	select {}
 }
